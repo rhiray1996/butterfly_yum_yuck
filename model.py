f330abd662857e37d474c838469ccb7b48384726
Rushikesh Hiray <rhiray1996@gmail.com>
2022-09-17 10:31:33 +0530

    Done

--- model.py
+++ model.py
@@ -13,22 +13,22 @@ class ModelCreation():
         
         encoder = ResNet50(weights="imagenet", include_top=False, input_tensor=inputs)
         
-        for layer in encoder.layers:
-            layer.trainable = False
+        # for layer in encoder.layers:
+        #     layer.trainable = False
         
         image_features = encoder.get_layer("conv4_block6_out").output
         
-        x = Conv2D(filters=32, kernel_size=3, padding='same', use_bias=False)(image_features)
-        x = Activation('relu')(x)
-        
-        
-        x = Conv2D(filters=64, kernel_size=3, use_bias=False)(x)
-        x = Activation('relu')(x)
-        
-        
-        x = Conv2D(filters=128, kernel_size=3, use_bias=False)(x)
-        x = Activation('relu')(x)
+        x = Conv2D(filters=256, kernel_size=3, padding='same', use_bias=False, activation="relu")(image_features)
         x = MaxPooling2D((2, 2), strides=2)(x),
+        
+        
+        # x = Conv2D(filters=64, kernel_size=3, padding='same', use_bias=False, activation="relu")(x)
+        # # x = Activation('relu')(x)
+        
+        
+        # x = Conv2D(filters=128, kernel_size=3, padding='same', use_bias=False, activation="relu")(x)
+        # # x = Activation('relu')(x)
+        # x = MaxPooling2D((2, 2), strides=2)(x),
         
         x = Flatten()(x[0]),
         x = Dense(64, activation='relu')(x[0]),
